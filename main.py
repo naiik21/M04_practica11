37a8a8f0f0c82dad6f7139d9c90c8e62deca77be
rule2003 <rllamas22@jaumebalmes.net>
2023-01-30 20:36:33 +0100

    ejercicios de Raul

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import exercici1_B as ex1B
 import exercici2_B as ex2B
 import exercici3_B as ex3B
+import exercici1_A as ex1A
+import exercici2_A as ex2A
+import exercici3_A as ex3A
 
 print(ex1B.mesGran())
 print(ex2B.parellOSenar())
 print(ex3B.declaracio())
+
+print(ex1A.masGrande())
+print(ex2A.nombre())
+print(ex3A.adivina())
